de47394d407e8bc4028db05f6ad2cb7014992548
vanbietnguyen <vanbietnguyen@gmail.com>
2021-10-04 13:56:48 -0400

    initialized project

--- frontend/client/app.jsx
+++ frontend/client/app.jsx
@@ -2,10 +2,7 @@ import React, { Suspense, lazy } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
-import Loader from './components/Loader/Loader';
-
-const Home = lazy(() => import('./views/Home'));
-const About = lazy(() => import('./views/About'));
+import Register from './views/Register'
 
 import './global.scss';
 
@@ -13,16 +10,14 @@ const App = () => {
   return (
     <Router>
       <Header />
-      <Suspense fallback={<Loader />}>
         <Switch>
-          <Route path="/about">
-            <About />
+          <Route path="/Register">
+            <Register />
           </Route>
           <Route path="/">
             <Home />
           </Route>
         </Switch>
-      </Suspense>
       <Footer className="footer" />
     </Router>
   );
---
+++ frontend/client/components/RegistrationForm/RegistrationForm.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const RegisterForm = () => {
+  return (
+    <>
+      <h1>registrationform</h1>
+    </>
+  );
+};
+
+export default RegisterForm;
